303b564c641278f4778660e0409b2b807e3f890c
Nepo Rojas <nepo.rojas@gmail.com>
2025-02-28 10:13:55 -0700

    ✅🗃️ Add example

--- tests/testthat/test_read_data.R
+++ tests/testthat/test_read_data.R
@@ -6,11 +6,18 @@ describe("Test 2006 data", {
     n_rows_expected <- 7
     expect_equal(n_rows_expected, n_rows_obtained)
   })
-    it("Read 2009 data", {
+  it("Read 2009 data", {
     path <- "/workdir/tests/data/diputados_2009.txt"
     obtained <- read_deputy_data(path)
     n_rows_obtained <- nrow(obtained)
     n_rows_expected <- 12
+    expect_equal(n_rows_expected, n_rows_obtained)
+  })
+  it("Read 2012 data", {
+    path <- "/workdir/tests/data/diputados_2012.txt"
+    obtained <- read_deputy_data(path)
+    n_rows_obtained <- nrow(obtained)
+    n_rows_expected <- 13
     expect_equal(n_rows_expected, n_rows_obtained)
   })
 })
