2d092545a1003c867ad7749f44715d857e1e8e49
Nepo Rojas <nepo.rojas@gmail.com>
2025-03-03 09:39:01 -0700

    ✅ Pass test

--- R/read_data.R
+++ R/read_data.R
@@ -2,3 +2,8 @@ read_deputy_data <- function(path_file) {
   diputaciones <- readr::read_delim(path_file, delim = "|", show_col_types = FALSE) |>
     janitor::clean_names()
 }
+
+read_deputy_data_2024 <- function(path_file) {
+  diputaciones <- readr::read_csv(path_file, show_col_types = FALSE) |>
+    janitor::clean_names()
+}
